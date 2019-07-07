7161fc419c05b686e2897245076626c2e771513b
Titu B G <titu.bg@gmail.com>
2019-07-07 22:53:25 +0530

    Updated code for right triangle to work in all scenarios
     - Added extra exercises
     - Modified other files to optimize declarations

--- week1/assignment/hello/CMYKtoRGB.java
+++ week1/assignment/hello/CMYKtoRGB.java
@@ -1,12 +1,5 @@
 public class CMYKtoRGB {
-
     public static void main(String[] args) {
-        double c = Double.parseDouble(args[0]), m = Double.parseDouble(args[1]), y = Double.parseDouble(args[2]), k = Double.parseDouble(args[3]);
-        double white = 1 - k;
-        long r = Math.round(255 * white * (1 - c)), g = Math.round(255 * white * (1 - m)), b = Math.round(255 * white * (1 - y));
-        System.out.println("red   = " + r);
-        System.out.println("green = " + g);
-        System.out.println("blue  = " + b);
+        System.out.println("red   = " + Math.round(255 * (1 - Double.parseDouble(args[3])) * (1 - Double.parseDouble(args[0]))) + "\ngreen = " + Math.round(255 * (1 - Double.parseDouble(args[3])) * (1 - Double.parseDouble(args[1]))) + "\nblue  = " + Math.round(255 * (1 - Double.parseDouble(args[3])) * (1 - Double.parseDouble(args[2]))));
     }
-
 }
--- week1/assignment/hello/RightTriangle.java
+++ week1/assignment/hello/RightTriangle.java
@@ -1,10 +1,10 @@
 public class RightTriangle {
     public static void main(String[] args) {
-        // Get the numbers
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        int c = Integer.parseInt(args[2]);
-        boolean result = (a*a == b*b+c*c) || (b*b == c*c+a*a) || (c*c == a*a+b*b);
+        long a = Long.parseLong(args[0]), b = Long.parseLong(args[1]), c = Long.parseLong(args[2]);
+        long max = Math.max(a, Math.max(b, c)), min = Math.min(a, Math.min(b, c)), mid = (a + b + c) - (min + max);
+        long maxs = max * max, mins = min * min, mids = mid * mid;
+        boolean sensibleNumber = (a > 0) && (b > 0) && (c > 0) && (maxs > 0) && (mins > 0) && (mids > 0) && ((mins + mids) > 0);
+        boolean result = sensibleNumber && (maxs == mins + mids);
         System.out.println(result);
     }
 }
